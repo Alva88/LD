f80325fdc5cc9fa642f3b25b801c63f0dd796e9f
Andretti Medina <andretti88@gmail.com>
2020-10-25 09:42:41 -0500

    Update loanDepotScript.ps1

--- loanDepotScript.ps1
+++ loanDepotScript.ps1
@@ -1,28 +1,28 @@
-﻿<#
+<#
 Hi Andretti,
 
 Per my voice message, please complete the take home assignment below, post to your github account,
- send the link to me, and I will send over to our DevOps Director to check, thanks.
+send the link to me, and I will send over to our DevOps Director to check, thanks.
 
 Create a powershell function or cmdlet that takes an arbitrarily long string parameter $foo 
 (e.g. 'abcdedcba') and returns the first recurring character in that Input string.
-In 'abcdedcba', 'd' would be the first recurring character, so the cmdlet/function should return 'd'
- for that sample input.
+In 'abcdedcba', 'd' would be the first recurring character, so the cmdlet/function should 
+return 'd' for that sample input.
 
 Be mindful of the runtime of your solution.
 #>
 
-function ReturnFirstReplicateChar($str) {
-    $CharArray = $str.ToCharArray()
+function ReturnFirstReplicateChar($foo) {
+    $ret = ""
     $tmpArray = @()
-        foreach($char in $CharArray) {  
-            
-            if ($tmpArray.Contains($char)) {                
-                return $char                
-                break
-            }            
-        $tmpArray += $char  
-    }         
+    foreach($char in $foo.ToCharArray()) {              
+        if ($tmpArray.Contains($char)) {                
+            $ret = $char                
+            break
+        }            
+    $tmpArray += $char  
+    }
+    return $ret         
 }
-$val = "abcddcbad"
-ReturnFirstReplicateChar($val)
+$foo = "abcddcba"
+ReturnFirstReplicateChar($foo)
